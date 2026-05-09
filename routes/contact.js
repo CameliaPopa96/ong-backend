71f14a521fdb2a0c6729dbc2457c452fc6b5ffba
CameliaPopa96 <127297435+CameliaPopa96@users.noreply.github.com>
2026-05-09 20:57:26 +0300

    Update contact.js

--- routes/contact.js
+++ routes/contact.js
@@ -1,1 +1,10 @@
+import express from "express";
 
+const router = express.Router();
+
+router.post("/", (req,res)=>{
+  console.log(req.body);
+  res.json({ success:true });
+});
+
+export default router;
