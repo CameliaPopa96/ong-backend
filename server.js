ed4ce0307fafc7914ad9c830f3f3669ac4f31143
CameliaPopa96 <127297435+CameliaPopa96@users.noreply.github.com>
2026-05-09 20:56:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
+import donations from "./routes/donations.js";
+import impact from "./routes/impact.js";
+import contact from "./routes/contact.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/donations", donations);
+app.use("/api/impact", impact);
+app.use("/api/contact", contact);
+
+app.get("/", (req,res)=>{
+  res.send("ONG Backend Running");
+});
+
+app.listen(process.env.PORT || 3000);
