cb82d5448a9b8c508f838b90c70ca215cdf5d2ee
CameliaPopa96 <127297435+CameliaPopa96@users.noreply.github.com>
2026-05-09 20:56:49 +0300

    Update donations.js

--- routes/donations.js
+++ routes/donations.js
@@ -1,1 +1,28 @@
+import express from "express";
+import { stripe } from "../config/stripe.js";
 
+const router = express.Router();
+
+router.post("/checkout", async (req,res)=>{
+
+  const { amount } = req.body;
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types:["card"],
+    mode:"payment",
+    line_items:[{
+      price_data:{
+        currency:"ron",
+        product_data:{ name:"Donație ONG Aiud" },
+        unit_amount: amount * 100
+      },
+      quantity:1
+    }],
+    success_url: process.env.FRONTEND_URL,
+    cancel_url: process.env.FRONTEND_URL
+  });
+
+  res.json({ url: session.url });
+});
+
+export default router;
