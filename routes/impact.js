f941f256a3304a51965f7b14af49fddc6682dea7
CameliaPopa96 <127297435+CameliaPopa96@users.noreply.github.com>
2026-05-09 20:57:07 +0300

    Update impact.js

--- routes/impact.js
+++ routes/impact.js
@@ -1,1 +1,14 @@
+import express from "express";
 
+const router = express.Router();
+
+router.get("/", (req,res)=>{
+
+  res.json({
+    patients: 523,
+    donations: 189320
+  });
+
+});
+
+export default router;
